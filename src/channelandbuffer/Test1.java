a1a27d7eddb5190d023cbc6e1824f838497f8fcd
wuyuan1002 <2461190767@qq.com>
2019-06-01 12:58:23 +0800

    新建文件

--- src/channelandbuffer/Test1.java
+++ src/channelandbuffer/Test1.java
@@ -1,15 +1,37 @@
 package channelandbuffer;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 /**
+ * 从NioTest1.txt中读取数据写到NioTest2.txt中
+ *
  * @author wuyuan
  * @version 1.0
  * @date 2019/6/1
  */
 public class Test1 {
     public static void main(String[] args) throws Exception {
+
         FileInputStream in = new FileInputStream("folder\\NioTest1");
-        FileInputStream out = new FileInputStream("folder\\NioTest2");
+        FileOutputStream out = new FileOutputStream("folder\\NioTest2");
+
+
+        FileChannel inchannel = in.getChannel();
+        FileChannel outchannel = out.getChannel();
+
+        ByteBuffer buffer = ByteBuffer.allocate(1024);
+
+        while (inchannel.read(buffer) != -1){
+            buffer.flip();
+            outchannel.write(buffer);
+            buffer.clear();
+        }
+
+        inchannel.close();
+        outchannel.close();
+
     }
 }
