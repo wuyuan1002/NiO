4f45bbaee5cc8752ba90f597cf07fd37475b47b6
wuyuan1002 <2461190767@qq.com>
2020-03-22 10:19:46 +0800

    nio

--- src/buffer/FileLockTest.java
+++ src/buffer/FileLockTest.java
@@ -7,16 +7,18 @@ import java.nio.channels.FileLock;
 /**
  * 文件锁
  *
- * RandomAccessFile：可以自由访问和修改文件的任意位置。他不是io体系的类，直接继承Object类。使用很多 native方法实现了对文件的操作。
- * 由于可以直接跳到文件的任意位置来读写数据，所以要在文件末尾增加数据时就不用把文件都读到内存中了。
+ * RandomAccessFile：（JDK1.4之后大多数方法被内存映射文件取代, 更推荐使用内存映射文件而不是RandomAccessFile）
  *
- * RandomAccessFile类包含了一个记录指针，用以标识当前读写处的位置，当程序新创建一个RandomAccessFile对象时，
- * 该对象的文件记录指针位于文件头（也就是0处）,当读/写了n个字节后，文件记录指针将会向后移动n个字节。
- * 除此之外，RandomAccessFile可以自由的移动记录指针，即可以向前移动，也可以向后移动。
- * RandomAccessFile包含了以下两个方法来操作文件的记录指针.
+ *      可以自由访问和修改文件的任意位置。他不是io体系的类，直接继承Object类。使用很多 native方法实现了对文件的操作。
+ *      由于可以直接跳到文件的任意位置来读写数据，所以要在文件末尾增加数据时就不用把文件都读到内存中了。
  *
- *      long getFilePointer(); 返回文件记录指针的当前位置
- *      void seek(long pos); 将文件记录指针定位到pos位置
+ *      RandomAccessFile类包含了一个记录指针，用以标识当前读写处的位置，当程序新创建一个RandomAccessFile对象时，
+ *      该对象的文件记录指针位于文件头（也就是0处）,当读/写了n个字节后，文件记录指针将会向后移动n个字节。
+ *      除此之外，RandomAccessFile可以自由的移动记录指针，即可以向前移动，也可以向后移动。
+ *      RandomAccessFile包含了以下两个方法来操作文件的记录指针.
+ *
+ *           long getFilePointer(); 返回文件记录指针的当前位置
+ *           void seek(long pos); 将文件记录指针定位到pos位置
  *
  * @author wuyuan
  * @date 2020/3/21
